98a62973510312acedd8f4d08c1b3d3d15153e1c
jammin316 <jammin316@gmail.com>
2019-07-02 01:34:44 -0700

    added Github link to footer

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -10,12 +10,12 @@ const modalBg = document.querySelectorAll(".modal-bg");
 // Assign click per Project Item and Close X Buttons
 for (let i = 0; i < itemText.length; i++) {
   // Click Project Item and show matching Modal
-  itemText[i].addEventListener("click", function() {
+  itemText[i].addEventListener("click", function () {
     modalBg[i].classList.remove("hide");
   });
 
   // Modal closes with matching X button
-  closeM[i].addEventListener("click", function() {
+  closeM[i].addEventListener("click", function () {
     modalBg[i].classList.add("hide");
   });
 }
